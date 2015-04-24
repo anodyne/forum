222cd2854325f6df304d0ef3e777fbe158d92de6
David VanScott <david.vanscott@gmail.com>
2015-04-23 20:13:22 -0400

    Add some config files

---
+++ config/anodyne.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+	'email' => [
+
+		'address'	=> 'contact@anodyne-productions.com',
+		'name'		=> 'Anodyne Productions',
+
+		'subject'	=> "[Anodyne Help Center]",
+
+		'admin'		=> 'admin@anodyne-productions.com',
+
+	],
+
+	'links' => [
+
+		'www'		=> 'http://anodyne-productions.com/',
+		'nova'		=> 'http://anodyne-productions.com/nova',
+		'forums'	=> 'http://forums.anodyne-productions.com/',
+		'xtras'		=> 'http://xtras.anodyne-productions.com/',
+
+	],
+
+];
---
+++ config/icons.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+
+	'back' => '<span class="icn" data-icon="<"></span>',
+	'bug' => '<span class="octicon octicon-bug"></span>',
+
+	'comment' => '<span class="icn" data-icon="c"></span>',
+	'comments' => '<span class="icn" data-icon="v"></span>',
+	'close' => '<span class="icn" data-icon="x"></span>',
+
+	'flag' => '<span class="icn" data-icon="f"></span>',
+
+	'markdown' => '<span class="octicon octicon-markdown"></span>',
+
+	'new' => '<span class="icn" data-icon="n"></span>',
+	'next' => '<span class="icn" data-icon=">"></span>',
+
+	'share' => '<span class="icn" data-icon="a"></span>',
+	'search-lg' => '<span class="octicon octicon-search octicon-size-lg"></span>',
+	//'star' => '<span class="octicon octicon-star"></span>',
+	'star' => '<span class="icn" data-icon="s"></span>',
+
+	'tag' => '<span class="icn" data-icon="t"></span>',
+	'thumbsUp' => '<span class="icn" data-icon="1"></span>',
+	'thumbsDown' => '<span class="icn" data-icon="2"></span>',
+
+	'user' => '<span class="icn" data-icon="p"></span>',
+
+];
