76771a5acaa9e329804571485d8fd803ef7c48cb
David VanScott <david.vanscott@gmail.com>
2015-05-07 21:30:46 -0400

    Show children the same way in the topic list as elsewhere

--- resources/views/partials/topics-list.blade.php
+++ resources/views/partials/topics-list.blade.php
@@ -10,5 +10,20 @@
 			{!! $topic->present()->discussionCount !!}
 		</div>
 	</div>
+
+	@if ($includeChildren)
+		@foreach ($topic->children as $child)
+			<div class="row">
+				<div class="col-md-10">
+					<p class="list-item-title updated">{!! $child->present()->nameAsLink !!}</p>
+					<span class="list-item-meta">{!! $child->present()->description !!}</span>
+					<p class="list-item-meta">{!! $child->present()->lastUpdate !!}</p>
+				</div>
+				<div class="col-md-2">
+					{!! $child->present()->discussionCount !!}
+				</div>
+			</div>
+		@endforeach
+	@endif
 @endforeach
 </div>
