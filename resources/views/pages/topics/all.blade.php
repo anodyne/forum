3d013b92bcba3e925c2901ed83e70833983a2917
David VanScott <david.vanscott@gmail.com>
2015-05-07 21:29:55 -0400

    Simplify All Topics listing

--- resources/views/pages/topics/all.blade.php
+++ resources/views/pages/topics/all.blade.php
@@ -7,22 +7,5 @@
 @section('content')
 	<h1>All Topics</h1>
 
-	<div class="row">
-		<div class="col-md-4 col-lg-3">
-			{!! partial('forum-controls', ['topics' => $allTopics, 'includeAllDiscussionsLink' => true, 'includeAllTopicsLink' => false]) !!}
-		</div>
-
-		<div class="col-md-8 col-lg-9">
-			{!! partial('topics-list', ['topics' => $topics]) !!}
-		</div>
-	</div>
-@stop
-
-@section('modals')
-	@if ($_currentUser)
-		{!! modal(['id' => 'newDiscussion', 'header' => "Start a Discussion"]) !!}
-	@endif
-@stop
-
-@section('scripts')
+	{!! partial('topics-list', ['topics' => $topics, 'includeChildren' => true]) !!}
 @stop
