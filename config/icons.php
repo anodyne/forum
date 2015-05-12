efa974eaca89e460fba95b875a2946c9f7e1f4a4
David VanScott <david.vanscott@gmail.com>
2015-05-12 16:38:07 -0400

    Font updates

--- config/icons.php
+++ config/icons.php
@@ -2,29 +2,36 @@
 
 return [
 
-	'back' => '<span class="icn" data-icon="<"></span>',
+	/*'back' => '<span class="icn" data-icon="<"></span>',
 	'bug' => '<span class="octicon octicon-bug"></span>',
 
 	'comment' => '<span class="icn" data-icon="c"></span>',
 	'comments' => '<span class="icn" data-icon="v"></span>',
-	'close' => '<span class="icn" data-icon="x"></span>',
+	'close' => '<span class="icn" data-icon="x"></span>',*/
 
-	'flag' => '<span class="icn" data-icon="f"></span>',
+	'check' => '<span class="icon-check"></span>',
+
+	'edit' => '<span class="icon-edit"></span>',
+
+	//'flag' => '<span class="icn" data-icon="f"></span>',
 
 	'markdown' => '<span class="octicon octicon-markdown"></span>',
 
-	'new' => '<span class="icn" data-icon="n"></span>',
-	'next' => '<span class="icn" data-icon=">"></span>',
+	//'new' => '<span class="icn" data-icon="n"></span>',
+	//'next' => '<span class="icn" data-icon=">"></span>',
 
-	'share' => '<span class="icn" data-icon="a"></span>',
+	'remove' => '<span class="icon-trash"></span>',
+
+	//'share' => '<span class="icn" data-icon="a"></span>',
 	'search-lg' => '<span class="octicon octicon-search octicon-size-lg"></span>',
 	//'star' => '<span class="octicon octicon-star"></span>',
-	'star' => '<span class="icn" data-icon="s"></span>',
+	//'star' => '<span class="icn" data-icon="s"></span>',
 
-	'tag' => '<span class="icn" data-icon="t"></span>',
-	'thumbsUp' => '<span class="icn" data-icon="1"></span>',
-	'thumbsDown' => '<span class="icn" data-icon="2"></span>',
+	//'tag' => '<span class="icn" data-icon="t"></span>',
+	//'thumbsUp' => '<span class="icn" data-icon="1"></span>',
+	//'thumbsDown' => '<span class="icn" data-icon="2"></span>',
 
-	'user' => '<span class="icn" data-icon="p"></span>',
+	//'user' => '<span class="icn" data-icon="p"></span>',
+	'user' => '<span class="icon-user"></span>',
 
 ];
