7f2fac2384710d6530ebc594cd19f8065330aa0d
David VanScott <david.vanscott@gmail.com>
2015-05-12 16:40:24 -0400

    Add the user controller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,23 @@
+<?php namespace Forums\Http\Controllers;
+
+use UserRepositoryInterface;
+use Forums\Http\Requests;
+use Illuminate\Http\Request;
+
+class UserController extends Controller {
+
+	protected $repo;
+
+	public function __construct(UserRepositoryInterface $repo)
+	{
+		parent::__construct();
+
+		$this->repo = $repo;
+	}
+
+	public function show($username)
+	{
+		//
+	}
+
+}
