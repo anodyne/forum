00eb8dac17059f4051bb3daf4fe6ba5a79b8b648
David VanScott <david.vanscott@gmail.com>
2015-05-12 22:14:17 -0400

    Re-write the post seeder
    This version is much better and will randomize whether a discussion is
    marked as answered. The only thing left to do would be to make sure no
    2 consecutive posts have the same author

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -13,25 +13,37 @@ class PostSeeder extends Seeder {
 	{
 		$faker = Faker\Factory::create();
 
-		$discussions = DiscussionSeeder::$number;
-		$posts = $discussions * 10;
-
-		for ($d = 1; $d <= $discussions; $d++)
+		foreach (Discussion::all() as $discussion)
 		{
-			Post::create([
-				'user_id' => $faker->numberBetween(1, User::count()),
-				'discussion_id' => $d,
-				'content' => $faker->realText(750),
-			]);
-		}
+			// Should this discussion have an answer?
+			$shouldBeAnswered = $faker->boolean();
 
-		for ($p = 0; $p < $posts; $p++)
-		{
-			Post::create([
-				'user_id' => $faker->numberBetween(1, User::count()),
-				'discussion_id' => $faker->numberBetween(1, $discussions),
-				'content' => $faker->realText(750),
-			]);
+			// An array for storing all the post IDs
+			$ids = [];
+
+			for ($p = 0; $p < $faker->numberBetween(1, 25); $p++)
+			{
+				$post = Post::create([
+					'user_id' => $faker->numberBetween(1, User::count()),
+					'discussion_id' => $discussion->id,
+					'content' => $faker->realText($faker->numberBetween(100, 1000)),
+				]);
+
+				if ($shouldBeAnswered and $p > 0)
+				{
+					$ids[] = $post->id;
+				}
+			}
+
+			if ($shouldBeAnswered and count($ids) > 0)
+			{
+				$key = $faker->numberBetween(0, (count($ids) - 1));
+
+				$discussion->update(['answer_id' => $ids[$key]]);
+			}
+
+			// Reset the list
+			$ids = [];
 		}
 	}
 
