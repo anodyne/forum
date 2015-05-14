910f578efd84650a29c6bd837150cf9a3dddff1b
David VanScott <david.vanscott@gmail.com>
2015-05-14 15:41:56 -0400

    Update the icomoon fonts

--- config/icons.php
+++ config/icons.php
@@ -1,17 +1,27 @@
 <?php
 
 return [
+	
+	'back' => '<span class="icon-arrow-back"></span>',
 
 	'check' => '<span class="icon-check"></span>',
 
 	'edit' => '<span class="icon-edit"></span>',
 
+	'forward' => '<span class="icon-arrow-forward"></span>',
+
+	'link' => '<span class="icon-link"></span>',
+
 	'markdown' => '<span class="octicon octicon-markdown"></span>',
+	'message' => '<span class="icon-message"></span>',
 
 	'remove' => '<span class="icon-trash"></span>',
 
 	'search-lg' => '<span class="octicon octicon-search octicon-size-lg"></span>',
+	'share' => '<span class="icon-share"></span>',
 
-	'user' => '<span class="icon-user"></span>',
+	'user' => '<span class="icon-person"></span>',
+
+	'warning' => '<span class="icon-warning"></span>',
 
 ];
