5c3cd81fedc81f0f2763462cd4200e05392d7686
David VanScott <david.vanscott@gmail.com>
2015-05-14 15:41:16 -0400

    Add form request for editing discussion

---
+++ app/Http/Requests/EditDiscussionRequest.php
@@ -0,0 +1,27 @@
+<?php namespace Forums\Http\Requests;
+
+use Auth;
+
+class EditDiscussionRequest extends Request {
+
+	/**
+	 * Determine if the user is authorized to make this request.
+	 *
+	 * @return bool
+	 */
+	public function authorize()
+	{
+		return true;
+	}
+
+	public function rules()
+	{
+		return [];
+	}
+
+	public function messages()
+	{
+		return [];
+	}
+
+}
