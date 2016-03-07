80059c97b48f9d63feec78ac1855f059061a2e48
David VanScott <david.vanscott@gmail.com>
2016-03-07 08:51:30 -0500

    Update some of the default data

--- database/migrations/2015_04_24_123950_create_topics_tables.php
+++ database/migrations/2015_04_24_123950_create_topics_tables.php
@@ -45,11 +45,19 @@ class CreateTopicsTables extends Migration {
 			['name' => "AnodyneXtras", 'slug' => "xtras", 'color' => "#d81b60", 'description' => ""],
 			['name' => "Help Center", 'slug' => "", 'color' => "#0288d1", 'description' => ""],
 			
-			['name' => "Article Discussions", 'slug' => "help-center-articles", 'color' => "#0288d1", 'description' => "", 'parent_id' => 4],
-			['name' => "Announcements", 'slug' => "", 'color' => "#607d8b", 'description' => "", 'parent_id' => 2],
+			// Nova sub topics
 			['name' => "Help", 'slug' => "nova-help", 'color' => "#259b24", 'description' => "", 'parent_id' => 1],
-			['name' => "Skinning", 'slug' => "nova-skinning", 'color' => "#259b24", 'description' => "", 'parent_id' => 1],
+			['name' => "Themeing", 'slug' => "nova-themeing", 'color' => "#259b24", 'description' => "", 'parent_id' => 1],
+			['name' => "Nova NextGen", 'slug' => "nova-nextgen", 'color' => "#259b24", 'description' => "", 'parent_id' => 1],
+
+			// Anodyne Lounge sub topics
+			['name' => "Announcements", 'slug' => "", 'color' => "#607d8b", 'description' => "", 'parent_id' => 2],
+
+			// AnodyneXtras sub topics
 			['name' => "Xtra Discussions", 'slug' => "xtras-discussion", 'color' => "#d81b60", 'description' => "", 'parent_id' => 3],
+
+			// Help Center sub topics
+			['name' => "Article Discussions", 'slug' => "help-center-articles", 'color' => "#0288d1", 'description' => "", 'parent_id' => 4],
 		];
 
 		foreach ($data as $d)
